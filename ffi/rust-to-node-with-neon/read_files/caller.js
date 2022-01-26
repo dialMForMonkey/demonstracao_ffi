ed072200f4bf91583e5108be908dbf5390035c47
Gabriel Souza <gabrielsouza849@gmail.com>
2022-01-25 23:32:59 -0300

    read files

--- ffi/rust-to-node-with-neon/read_files/caller.js
+++ ffi/rust-to-node-with-neon/read_files/caller.js
@@ -1,8 +1,62 @@
-const {readFiles}  = require('./index');
-debugger
-console.log(
-    readFiles([
-        "./frase1.txt"
-    ])
-)
+const 
+  r
+ = require('./index');
+const fs = require('fs');
+
+const files = [
+    "./frase1.txt",
+    "./frase2.txt",
+    "./frase3.txt",
+    "./frase4.txt"
+];
+
+/**
+    readFiles(files)
+**/
+
+
+function ErroReadFileMoreOneGB() {
+  fs.readFile("./frase1.txt", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log('acabou de ler arquivo 1');
+  });
+}
+
+function readFile(path, finishFun){
+    const streamFile = fs.createReadStream("./frase1.txt");
+    streamFile.on('error', (err) => {
+        console.log('err', err);
+    })
+    streamFile.on('readable', () => {
+      const read = streamFile.read({size:512});
+      if(read == null) {
+        finishFun()
+      } 
+    })
+    streamFile.on('ready', () => {
+        console.log('ready for read file ',path);
+    })
+}
+
+
+function readFilesNode(files){
+    for (let index = 0, max= files.length; index < max; index++) {
+      const file = files[index];
+      const start = Date.now();
+      readFile(file,()=>{
+
+        const end = Date.now();
+        const elapsed = end - start;
+
+        console.log(`file ${file} time elapsed ${elapsed} ms`)
+      })
+    }
+}
+
+//r.readFiles(files);
+
+readFilesNode(files);
+
 
--- ffi/rust-to-node-with-neon/read_files/src/lib.rs
+++ ffi/rust-to-node-with-neon/read_files/src/lib.rs
@@ -1,38 +1,47 @@
-use std::io::{Error, Read};
-use std::thread;
+use std::{thread, time};
+use async_std::fs::File;
+use async_std::io::BufReader;
+use async_std::task;
 use neon::prelude::*;
-use tokio::fs::File;
-use tokio::fs::file::OpenFuture;
-use tokio::prelude::{AsyncRead, Future};
-use tokio::prelude::future::{AndThen, MapErr};
+use async_std::prelude::*;
+use rayon::prelude::*;
 
+const LEN: usize = 7815744000;
+fn read_file_async(path: String)  {
 
-fn read_file(path: String) {
-
-    let task=
-        tokio::fs::read(path.clone())
-        .map(move |mut contents| {
-
-
-            println!("path {} {}", String::from_utf8(contents).unwrap(), path);
-        })
-        .map_err(|err| println!("IO error: {:?}", err));
-
-    tokio::run(task);
+    task::block_on(async {
+        let mut file = File::open(&path).await.unwrap();
+        let mut br = BufReader::new(file);
+        let mut buf =  vec![0u8; LEN];
+        loop {
+            let n =  br.read(&mut buf).await.unwrap();
+            if n == 0usize {
+                println!("{:?}", thread::current().id());
+                break  ;
+            }
+        }
+    });
 }
-
 
 fn read_files(mut cx: FunctionContext) -> JsResult<JsNumber> {
     let js_arr_handle: Handle<JsArray> = cx.argument(0).unwrap();
     let files_handler_js = js_arr_handle.to_vec(&mut cx).unwrap() ;
 
-    for file_value_js in files_handler_js {
-        let file_name  =  file_value_js.to_string(&mut cx).unwrap().value(&mut cx);
-        thread::spawn(|| {
-            println!("thread");
-            read_file(file_name);
+    files_handler_js
+        .iter()
+        .map(|&i|{
+            let file_name  =  i.to_string(&mut cx).unwrap().value(&mut cx);
+            file_name
+        })
+        .collect::<Vec<String>>()
+        .par_iter()
+        .for_each(|file_name| {
+            println!("ready for read file {}",file_name);
+            let start = time::Instant::now();
+            read_file_async(file_name.clone());
+            println!("file {} time elapsed {} ms",file_name, start.elapsed().as_millis());
         });
-    }
+
     Ok(cx.number(0f64))
 }
 
